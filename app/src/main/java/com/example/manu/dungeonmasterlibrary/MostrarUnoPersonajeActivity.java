afebe9d998bced1b5c567eaf8f0a9175cc3d7850
Kaukenas <manudiaz97@gmail.com>
2018-05-07 19:27:22 +0200

    Reimon

--- app/src/main/java/com/example/manu/dungeonmasterlibrary/MostrarUnoPersonajeActivity.java
+++ app/src/main/java/com/example/manu/dungeonmasterlibrary/MostrarUnoPersonajeActivity.java
@@ -1,18 +1,30 @@
 package com.example.manu.dungeonmasterlibrary;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MostrarUnoPersonajeActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
+    ImageButton imageButtonDados1;
+    double numero = 12.0;
+    TextView txtResultadoBoton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +33,10 @@ public class MostrarUnoPersonajeActivity extends AppCompatActivity {
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         ViewGroup inclusionViewGroup = findViewById(R.id.linearLayout3);
+        imageButtonDados1 = findViewById(R.id.imageButtonDados1);
+        txtResultadoBoton = findViewById(R.id.txtResultadoBoton);
+
+        txtResultadoBoton.setText((int) numero);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -41,5 +57,35 @@ public class MostrarUnoPersonajeActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        imageButtonDados1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tirarDadosButton();
+            }
+        });
     }
+
+    public void tirarDadosButton(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Este es el resultado de tu tirada");
+        // Get the layout inflater
+        LayoutInflater inflater = this.getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.dialog_signin, null));
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MostrarUnoPersonajeActivity.this, "Has efectuado tu tirada", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Dialog dialog = builder.create();
+        dialog.show();
+
+    }
+
+
 }
